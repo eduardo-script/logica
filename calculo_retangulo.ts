4a5ef09d5df306739c115bab18bff1171772ff0a
eduardo-script <eduardovinicius.code@gmail.com>
2026-04-20 16:59:40 -0300

    att atividades para casa

--- calculo_retangulo.ts
+++ calculo_retangulo.ts
@@ -2,7 +2,15 @@ const tela = require('readline-sync')
 
 const largura:number = Number(tela.question('Informe a largura: '))
 const altura:number = Number(tela.question('Informe a altura: '))
+ 
 
-//soma
-console.log("Soma de altura: "+largura)
-console.log("Soma de largura: "+altura)
+ if (isNaN(largura) || isNaN(altura)) {
+      console.log('Por favor, insira números válidos.');
+    } else {
+      const area = largura * altura;
+      const perimetro = 2 * (largura + altura);
+
+      console.log(`\n--- Resultados ---`);
+      console.log(`Área: ${area}`);
+      console.log(`Perímetro: ${perimetro}`);
+    }
